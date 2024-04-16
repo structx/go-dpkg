cfb7780d9445060186ecbaa83b191abaa9c40fdf
Trevor Atkinson <tsa@structx.io>
2024-04-16 17:09:50 -0600

    update logger (#2)

--- logging/zap.go
+++ logging/zap.go
@@ -1,9 +1,43 @@
 // Package logging zap logger
 package logging
 
-import "go.uber.org/zap"
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
 
-// NewLogger return new zap logger
-func NewLogger() (*zap.Logger, error) {
-	return zap.NewProduction()
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// NewLoggerFromEnv return uber/zap logger from environment
+func NewLoggerFromEnv() (*zap.Logger, error) {
+
+	logFile := os.Getenv("LOG_PATH")
+	if logFile == "" {
+		return nil, errors.New("$LOG_PATH must be set")
+	}
+
+	level := strings.ToLower(os.Getenv("LOG_LEVEL"))
+	if level == "" {
+		return nil, errors.New("$LOG_LEVEL must be set")
+	}
+
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = []string{
+		filepath.Clean(logFile),
+	}
+
+	var l zapcore.Level
+	switch level {
+	case "debug":
+		l = zap.DebugLevel
+	case "error":
+		l = zap.ErrorLevel
+	}
+
+	cfg.Level = zap.NewAtomicLevelAt(l)
+
+	return cfg.Build()
 }
--- logging/zap_test.go
+++ logging/zap_test.go
@@ -1,14 +1,20 @@
 package logging_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/trevatk/go-pkg/logging"
 )
 
+func init() {
+	_ = os.Setenv("LOG_LEVEL", "DEBUG")
+	_ = os.Setenv("LOG_PATH", "pkg.log")
+}
+
 func Test_NewLoggerFromEnv(t *testing.T) {
 	t.Run("provider", func(t *testing.T) {
-		logger, err := logging.NewLogger()
+		logger, err := logging.NewLoggerFromEnv()
 		if err != nil {
 			t.Fatalf("failed to initialize new logger %v", err)
 		}
