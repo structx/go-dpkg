0e63ae917da3ec53129366691bfce12e502a63cc
Trevor Atkinson <tsa@structx.io>
2024-04-27 23:46:15 -0600

    add server provider (#25)
    * add server provider
    
    * fix linting errors

---
+++ adapter/port/http/serverfx/http_server.go
@@ -0,0 +1,23 @@
+// Package serverfx http server provider
+package serverfx
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/trevatk/go-pkg/domain"
+)
+
+// New http server provider constructor
+func New(cfg domain.Config, handler http.Handler) *http.Server {
+	scfg := cfg.GetServer()
+	return &http.Server{
+		Addr:         net.JoinHostPort(scfg.BindAddr, fmt.Sprintf("%d", scfg.Ports.HTTP)),
+		Handler:      handler,
+		ReadTimeout:  time.Duration(scfg.DefaultTimeout) * time.Second,
+		WriteTimeout: time.Duration(scfg.DefaultTimeout) * time.Second,
+		IdleTimeout:  time.Duration(scfg.DefaultTimeout) * time.Second,
+	}
+}
--- adapter/storage/kv/testfiles/pebble.test.hcl
+++ adapter/storage/kv/testfiles/pebble.test.hcl
@@ -1,7 +1,7 @@
 
 server {
     bind_addr = "0.0.0.0"
-
+    default_timeout = 15
     ports {
         http = 8080
         grpc = 50051
