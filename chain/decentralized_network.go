aa1502f6d175953d31dfebd338161a50d4752467
trevor atkinson <tsa@structx.io>
2024-05-01 15:03:32 -0600

    define service interfaces

---
+++ chain/decentralized_network.go
@@ -0,0 +1,27 @@
+package chain
+
+// interfaces in this package are used to define
+// required service functionality to be used by platforms
+// above the decentralized network
+//
+// will define as empty to begin
+
+// AccessControl access control required functionality
+//
+//go:generate mockery --name AccessControl
+type AccessControl interface{}
+
+// DDNS decentralized dns required functionality
+//
+//go:generate mockery --name DDNS
+type DDNS interface{}
+
+// LightNode light node required functionality
+//
+//go:generate mockery --name LightNode
+type LightNode interface{}
+
+// TxManager transaction management required functionality
+//
+//go:generate mockery --name TxManager
+type TxManager interface{}
